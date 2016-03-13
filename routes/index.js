ea6de63cfc27180f0a1a5ee49f379977e236eec9
dettac <345098184@qq.com>
2016-03-13 21:04:08 +0800

    add archive modules

--- routes/index.js
+++ routes/index.js
@@ -137,6 +137,22 @@ module.exports = function (app){
 		res.redirect('/');
 	});
 
+	app.get('/archive',function(req, res){
+		Post.getArchive(function(err,posts){
+			if(err){
+				req.flash('error',err);
+				return res.redirect('/');
+			}
+			res.render('archive',{
+				title: 'Archives',
+				posts: posts,
+				user: req.session.user,
+				success: req.flash('success').toString(),
+				error: req.flash('error').toString()
+			});
+		});
+	});
+
 	app.get('/u/:name',function(req,res){
 		var page = req.query.p ? parseInt(req.query.p) : 1;
 		User.get(req.params.name,function(err,user){
